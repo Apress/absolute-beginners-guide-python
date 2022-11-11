99df9c3a95386da78fa4a99792dd23cc5074a663
Kevin Wilson <office@elluminetpress.com>
2022-11-11 12:45:08 +0000

    Add files via upload

---
+++ Chapter 09/polyclass2.py
@@ -0,0 +1,50 @@
+from math import pi
+
+
+class Polygon:
+    def __init__(self, name):
+        self.name = name
+    def getArea(self):
+        pass
+    def getName(self):
+        return "Polygon"
+    def __str__(self):
+        return self.name
+
+
+class Square(Polygon):
+    def __init__(self, length):
+        super().__init__("Square")
+        self.length = length
+    def getArea(self):
+        return self.length**2
+    def getName(self):
+        return "Square"
+
+
+class Circle(Polygon):
+    def __init__(self, radius):
+        super().__init__("Circle")
+        self.radius = radius
+    def getArea(self):
+        return pi*self.radius**2
+    def getName(self):
+        return "Circle"
+
+
+squareObj = Square(7)
+circleObj = Circle(7)
+
+
+print(circleObj.name)
+
+print(squareObj.name)
+
+print(circleObj.getName())
+
+print(squareObj.getArea())
+
+print(squareObj.getName())
+
+print(circleObj.getArea())
+
